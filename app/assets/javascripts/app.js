84c1f743a549366a5389d23a69ad631f61ed2256
dkim6002 <Danielkim629@gmail.com>
2014-11-20 17:21:02 -0500

    dashboard bills addition

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -52,31 +52,30 @@ $(document).ready(function(){
 
 		}
 
-		WePay.set_endpoint("stage"); // stage or production
+		// WePay.set_endpoint("stage"); // stage or production
 
-		WePay.OAuth2.button_init(document.getElementById('start_oauth2'), {
-		    "client_id":"190452",
-		     "scope":["manage_accounts","collect_payments","view_user","send_money","preapprove_payments"],
-		    "user_name":"test user",
-		    "user_email":"test@example.com",
-		    "redirect_uri":"http://localhost:3000/auth/:provider/callback",
-		    "top":100, // control the positioning of the popup with the top and left params
-		    "left":100,
-		    "state":"robot", // this is an optional parameter that lets you persist some state value through the flow
-		    "callback":function(data) {
-		    	console.log(data[0])
-				/** This callback gets fired after the user clicks "grant access" in the popup and the popup closes. The data object will include the code which you can pass to your server to make the /oauth2/token call **/
-				if (data.code.length !== 0) {
-					// send the data to the server
-				} else {
-					// an error has occurred and will be in data.error
-				}
-			}
-		});
+		// WePay.OAuth2.button_init(document.getElementById('start_oauth2'), {
+		//     "client_id":"190452",
+		//      "scope":["manage_accounts","collect_payments","view_user","send_money","preapprove_payments"],
+		//     "user_name":"test user",
+		//     "user_email":"test@example.com",
+		//     "redirect_uri":"http://localhost:3000/auth/:provider/callback",
+		//     "top":100, // control the positioning of the popup with the top and left params
+		//     "left":100,
+		//     "state":"robot", // this is an optional parameter that lets you persist some state value through the flow
+		//     "callback":function(data) {
+		//     	console.log(data[0])
+		// 		/** This callback gets fired after the user clicks "grant access" in the popup and the popup closes. The data object will include the code which you can pass to your server to make the /oauth2/token call **/
+		// 		if (data.code.length !== 0) {
+		// 			// send the data to the server
+		// 		} else {
+		// 			// an error has occurred and will be in data.error
+		// 		}
+		// 	}
+		// });
 
 		$('input[type=checkbox]').change(function(){
-		
-			$(this).parent().toggleClass('active');
+			$(this).parent().toggleClass('chore-list active');
 			
 		})
 
